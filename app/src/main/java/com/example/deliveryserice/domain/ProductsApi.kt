f45b9f63e999f6ff6a7b6c1caa739924e878a878
kamol <kamoladdinozadov24@gmail.com>
2022-03-21 13:29:14 +0300

    Delivery Service

---
+++ app/src/main/java/com/example/deliveryserice/domain/ProductsApi.kt
@@ -0,0 +1,9 @@
+package com.example.deliveryserice.domain
+
+data class ProductsApi(
+    val id : Int ,
+    val title : String,
+    val img : String ,
+    val price : Int ,
+    val boss : Int
+)
